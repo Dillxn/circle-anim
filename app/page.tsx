e5fc02ebc8d81985b0e83929addd69e25c0b6b27
Dillon Riecke <dillon.riecke@gmail.com>
2025-08-21 02:35:53 +0000

    circles infinite scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,109 @@
+import Image from 'next/image';
+import circle from '@/public/circle.svg';
+
 export default function Home() {
+  const opacities = Array(128)
+    .fill(0)
+    .map(() => Math.random() * 80 + 20);
   return (
     <>
-    
+      {/* container */}
+      <div
+        className="mt-24 h-128 overflow-hidden bg-white
+          perspective-distant"
+      >
+        <div className="absolute top-1/2 left-1/2 -translate-1/2">
+          <style>{`
+          @keyframes slide {
+            from {
+              translate: 0%;
+            }
+
+            to {
+              translate: -25%;
+            }
+          }
+        `}</style>
+          <div
+            className="flex w-fit"
+            style={{
+              animation: '10s linear slide infinite',
+            }}
+          >
+            <div
+              className="mr-[20px] grid gap-[20px]"
+              style={{
+                gridTemplateColumns: 'repeat(16, 1fr)',
+              }}
+            >
+              {opacities.map((opacity, index) => (
+                <Image
+                  key={index}
+                  alt="circle"
+                  src={circle}
+                  className="w-[100px] max-w-[unset]"
+                  style={{
+                    opacity: `${opacity}%`,
+                  }}
+                />
+              ))}
+            </div>
+            <div
+              className="mr-[20px] grid gap-[20px]"
+              style={{
+                gridTemplateColumns: 'repeat(16, 1fr)',
+              }}
+            >
+              {opacities.map((opacity, index) => (
+                <Image
+                  key={index}
+                  alt="circle"
+                  src={circle}
+                  className="w-[100px] max-w-[unset]"
+                  style={{
+                    opacity: `${opacity}%`,
+                  }}
+                />
+              ))}
+            </div><div
+              className="mr-[20px] grid gap-[20px]"
+              style={{
+                gridTemplateColumns: 'repeat(16, 1fr)',
+              }}
+            >
+              {opacities.map((opacity, index) => (
+                <Image
+                  key={index}
+                  alt="circle"
+                  src={circle}
+                  className="w-[100px] max-w-[unset]"
+                  style={{
+                    opacity: `${opacity}%`,
+                  }}
+                />
+              ))}
+            </div>
+            <div
+              className="mr-[20px] grid gap-[20px]"
+              style={{
+                gridTemplateColumns: 'repeat(16, 1fr)',
+              }}
+            >
+              {opacities.map((opacity, index) => (
+                <Image
+                  key={index}
+                  alt="circle"
+                  src={circle}
+                  className="w-[100px] max-w-[unset]"
+                  style={{
+                    opacity: `${opacity}%`,
+                  }}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
